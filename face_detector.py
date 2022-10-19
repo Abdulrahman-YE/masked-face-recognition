ca00bbaf2313dbcdb146a2c1404c28e41f241e33
Abdulrahman-YE <abdulrahman1ye@gmail.com>
2022-10-19 18:40:42 +0300

    اصلاح خطاء املائي

--- face_detector.py
+++ face_detector.py
@@ -4,7 +4,7 @@ import cv2
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-class FaceDectector(object):
+class FaceDetector(object):
     
     def __init__(self,resize=1, *args, **kwargs):
         self.mtcnn = MTCNN(*args, **kwargs)

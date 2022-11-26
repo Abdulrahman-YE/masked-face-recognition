d400a524bf4be053143de6bdee1ff1558a5a7bb6
Abdulrahman-YE <abdulrahman1ye@gmail.com>
2022-11-26 22:23:47 +0300

    Add logger

---
+++ utils/logger.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import logging
+
+
+_ROOT_LOGGER_NAME = 'MaskDetector'
+
+_ROOT_LOGGER = None
+
+
+def get_root_logger():
+
+    global _ROOT_LOGGER
+
+    if _ROOT_LOGGER is None:
+
+        _ROOT_LOGGER = logging.getLogger(_ROOT_LOGGER_NAME)
+        _ROOT_LOGGER.setLevel(logging.DEBUG)
+
+        formatter = logging.Formatter(fmt='(%(asctime)s) — [%(name)s] — [%(levelname)s] [*] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S')
+
+        handler = logging.FileHandler('%s.log' % _ROOT_LOGGER_NAME)
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(formatter)
+        _ROOT_LOGGER.addHandler(handler)
+
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+        handler.setFormatter(formatter)
+        _ROOT_LOGGER.addHandler(handler)
+
+    return _ROOT_LOGGER
+
+
+def get_logger(name):
+    return logging.getLogger('%s.%s' % (get_root_logger().name, name))
+
+def shutdown():
+    logging.shutdown()
